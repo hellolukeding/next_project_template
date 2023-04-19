4ef272684baee8720fb17bca6b078d2c78291df7
lukeding <dingkai9627@outlook.com>
2023-04-19 18:35:01 +0800

    feat✨(custom): git

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { getConfig } from './utils';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  
+  getConfig() {
+    return getConfig()
   }
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,4 @@
-import fs from 'fs';
-import path from 'path';
-import { parse } from 'yaml';
+import { parse } from "yaml"
 
 // 获取项目运行环境
 export const getEnv = () => {
@@ -8,9 +6,18 @@ export const getEnv = () => {
 }
 
 // 读取项目配置
+/**
+ * @description 读取项目配置
+ * @parse https://stackoverflow.com/questions/43112619/typescript-require-statement-not-part-of-an-import-statement
+ * @returns 
+ */
 export const getConfig = () => {
   const environment = getEnv()
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const path=require("path")
   const yamlPath = path.join(process.cwd(), `./.config/.${environment}.yaml`)
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const fs=require("fs")
   const file = fs.readFileSync(yamlPath, 'utf8')
   const config = parse(file)
   return config
