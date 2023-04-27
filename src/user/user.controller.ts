7efb881ea1c2de781c2280428cc76d0da7e15a6c
lukeding <dingkai9627@outlook.com>
2023-04-27 11:06:05 +0800

    feat✨(custom): 数据库配置

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+
 
 @Controller('user')
 export class UserController {
+
+  constructor(
+    private readonly userService: UserService,
+  ) { }
 
   @Get()
   getHello(): string {
